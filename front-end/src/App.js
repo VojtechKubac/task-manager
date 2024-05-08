bdb6393fb1b074a8524602bb8709dac8d2f0a232
Vojtech Kubac <vojta.kubac@gmail.com>
2024-05-08 15:16:58 +0200

    basic routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
-import TasksList from "./containers/TasksList";
+import BaseRouter from "./routes";
 
 
 class App extends Component {
 	render () {
 		return (
-			<>
-			<TasksList/>
-			</>
+			<Router>
+				<ul className="App-header">
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/login">
+							Log in
+						</Link>
+					</li>
+					<li>
+						<Link to="/tasks">
+							See tasks
+						</Link>
+					</li>
+				</ul>
+				<BaseRouter/>
+			</Router>
 		);
 	}
 }

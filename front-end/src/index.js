516452564be674d61ad77c3327aed69055a6daab
Vojtech Kubac <vojta.kubac@gmail.com>
2024-05-10 09:26:40 +0200

    cleaning in src/index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,32 +1,8 @@
-//import ReactDOM from 'react-dom/client';
-//import './index.css';
-//import reportWebVitals from './reportWebVitals';
-
-/*
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-*/
-
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-//import registerServiceWorker from "./registerServiceWorker";
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
-//import configureStore from 'redux';
-//import legacy_createStore from 'redux';
 import { Provider } from "react-redux";
-//import { composeWithDevTools } from 'redux-devtools-extension'; // Import composeWithDevTools from redux-devtools-extension
 import { thunk } from "redux-thunk";
 
 import tasksReducer from "./store/reducers/tasks";
@@ -39,9 +15,6 @@ const rootReducer = combineReducers({
   tasks: tasksReducer
 });
 
-//const store = configureStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-//const store = configureStore(rootReducer, composeEnhances(applyMiddleware(thunk)));
-//const store = legacy_createStore(rootReducer, composeEnhances(applyMiddleware(thunk)));
 const store = createStore(rootReducer, composeEnhances(applyMiddleware(thunk)));
 
 const app = (
@@ -49,13 +22,6 @@ const app = (
     <App />
   </Provider>
 );
-
-//ReactDOM.render(app, document.getElementById("root"));
-//registerServiceWorker();
-
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(app);
